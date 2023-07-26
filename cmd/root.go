a671999ef019a258fd59a9573fc8d3e21fc0b115
rh5661 <rh5661@rit.edu>
2023-07-26 10:15:50 -0400

    db initialization & query for creating tables

--- cmd/root.go
+++ cmd/root.go
@@ -4,12 +4,103 @@ Copyright © 2023 ROBERT HUANG
 package cmd
 
 import (
+	"database/sql"
 	"os"
 
 	"github.com/spf13/cobra"
 
 	_ "modernc.org/sqlite"
 )
+
+const dbInit string = `
+DROP TABLE IF EXISTS matrix;
+
+CREATE TABLE IF NOT EXISTS matrix (
+	id 					INTEGER NOT NULL PRIMARY KEY,
+	contract_start 		DATE NOT NULL,
+	state_code_id 		INTEGER NOT NULL,
+	util_code_id		INTEGER NOT NULL,
+	util_zone			TEXT NOT NULL,
+	util_rate_code		TEXT,
+	product_option		TEXT NOT NULL,
+	billing_method 		TEXT NOT NULL,
+	contract_term		INTEGER NOT NULL,
+	usage_lower			DOUBLE NOT NULL,
+	usage_middle 		DOUBLE NOT NULL,
+	usage_upper 		DOUBLE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS state_codes (
+    id				INTEGER NOT NULL PRIMARY KEY,
+    state_code		TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS util_codes (
+    id				INTEGER NOT NULL PRIMARY KEY,
+    util_code		TEXT NOT NULL,
+	util_name		TEXT NOT NULL
+);
+
+INSERT INTO state_codes (state_code) VALUES 
+	("DE"),
+	("IL"),
+	("MA"),
+	("MD"),
+	("ME"),
+	("NH"),
+	("NJ"),
+	("OH"),
+	("PA"),
+	("RI");
+
+INSERT INTO util_codes (util_code,util_name) VALUES
+	("AECO",	"Atlantic City Electric Company"),			
+	("APS",		"Potomac Edison - Allegheny Power"),			
+	("BED",		"Eversource - Boston Edison"), 			
+	("BGE",		"Baltimore Gas and Electric"),			
+	("CAMB",	"Eversource - Cambridge Electric"), 			
+	("CEI",		"Cleveland Electric"),			
+	("CGE",		"Duke Energy"),			
+	("CHGE",	"Central Hudson"),			
+	("CILCO",	"Ameren Rate Zone II - CILCO"),			
+	("CIPS",	"Ameren Rate Zone I - CIPS"),			
+	("CMP",		"Central Maine Power"),			
+	("COME",	"Eversource - Commonwealth Electric"),			
+	("COMED",	"Commonwealth Edison"),			
+	("CONE",	"Consolidated Edison"),			
+	("CS",		"AEP - CS"),			
+	("DELM",	"Conectiv Delmarva"),			
+	("DELMDE",	"Delmarva"),			
+	("DLCO",	"Duquesne Light Company"),
+	("DPL",		"Dayton Power and Light"),
+	("FGE",		"Unitil - Fitchburg Gas and Electric"),			
+	("GSECO",	"Granite State Electric Co (Liberty Utilities)"),			
+	("ILPWR",	"Ameren Rate Zone III - IP"),			
+	("JCPL",	"Jersey Central Power & Light Company"),			
+	("METED",	"Metropolitan Edison Company"),			
+	("MSEL",	"National Grid - Massachusetts Electric Company"),			
+	("MWST",	"Eversource - Western Massachusetts Electric"),			
+	("NECO",	"National Grid - Narragansett Electric"),			
+	("NHEC",	"New Hampshire Electric Co"),			
+	("NIMO",	"National Grid - Niagara Mohawk"),			
+	("NYOR",	"Orange and Rockland"),			
+	("NYSEG",	"New York State Electric & Gas"),			
+	("OE",		"Ohio Edison"),		
+	("OPCO",	"AEP - OP"),			
+	("PECO",	"PECO Energy"),			
+	("PENELEC",	"Pennsylvania Electric Company"),			
+	("PEPCO",	"Potomac Electric Power Company"),			
+	("PP",		"Pennsylvania Power Company"),			
+	("PPL",		"Pennsylvania Power and Light, Inc."),			
+	("PSEG",	"Public Service Electric and Gas Company"),			
+	("PSNH",	"Public Service Of New Hampshire"),			
+	("RECO",	"Rockland Electric Company"),			
+	("RGE",		"Rochester Gas & Electric"),			
+	("TE",		"Toledo Edison"),			
+	("UNITIL",	"Unitil Energy Systems"),			
+	("WPP",		"Allegheny Power WPP");
+
+`
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -29,6 +120,24 @@ to quickly create a Cobra application.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+
+	// open db
+	db, openErr := sql.Open("sqlite", "./data.db")
+	if openErr != nil {
+		os.Exit(1)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+
+		}
+	}(db)
+
+	_, createErr := db.Exec(dbInit)
+	if createErr != nil {
+		os.Exit(1)
+	}
+
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
