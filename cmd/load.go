a3ead86903e9af7621add2f2c4a433c5bc8ed3ab
rh5661 <rh5661@rit.edu>
2023-07-28 15:58:08 -0400

    added functionality to parse a row of excel

--- cmd/load.go
+++ cmd/load.go
@@ -5,24 +5,38 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/rh5661/matrixTool/pkg/excel"
 
 	"github.com/spf13/cobra"
 )
 
 // loadCmd represents the load command
-var loadCmd = &cobra.Command{
-	Use:   "load",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
+var (
+	filePath string
+
+	loadCmd = &cobra.Command{
+		Use:   "load",
+		Short: "A brief description of your command",
+		Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("load called")
-	},
-}
+		Run: func(cmd *cobra.Command, args []string) {
+			if filePath == "" && len(args) != 0 {
+				filePath = args[0]
+				fmt.Println("The entered filePath is: " + filePath + "\n")
+			} else {
+				fmt.Println("Please specify a filepath. Run 'matrixTool load --help' for more information.")
+				return
+			}
+
+			excel.ReadExcelFile(filePath)
+			fmt.Println("\nFinished loading")
+		},
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(loadCmd)
@@ -31,7 +45,7 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// loadCmd.PersistentFlags().String("foo", "", "A help for foo")
+	loadCmd.PersistentFlags().StringVarP(&filePath, "filePath", "", "", "[Required] Set filepath of matrix pricing Excel file to be parsed")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
