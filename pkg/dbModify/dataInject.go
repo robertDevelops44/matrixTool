33e052a9d20a1b6ad349b6ee8bf0336401bfd3bf
rh5661 <rh5661@rit.edu>
2023-07-28 16:50:40 -0400

    added functionality for inserting an excel row record into SQLite database

--- pkg/dbModify/dataInject.go
+++ pkg/dbModify/dataInject.go
@@ -1,6 +1,11 @@
 package dbModify
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"github.com/spf13/cobra"
+	"strconv"
+)
 
 func ProcessRow(row []string) bool {
 
@@ -18,16 +23,34 @@ func ProcessRow(row []string) bool {
 	usageMiddle = row[9]
 	usageUpper = row[10]
 
-	fmt.Printf(`Contract Start Month: %s
-State: %s
-Utility: %s
-Zone: %s
-Rate Code(s): %s
-Product Special Options: %s
-Billing Method: %s
-Term: %s
-0-49: %s
-50-299: %s
-300-1099: %s`, contractStart, state, utility, zone, rateCodes, productOptions, billingMethod, term, usageLower, usageMiddle, usageUpper)
+	db, openErr := sql.Open("sqlite", "./data.db")
+	cobra.CheckErr(openErr)
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		cobra.CheckErr(err)
+	}(db)
+	stmt, err := db.Prepare(`INSERT INTO matrix (contract_start, state_code_id, util_code_id, util_zone, util_rate_code, product_option, billing_method, contract_term, usage_lower, usage_middle, usage_upper) 
+	VALUES (?,?,?,?,?,?,?,?,?,?,?);`)
+	cobra.CheckErr(err)
+	res, err := stmt.Exec(contractStart, state, utility, zone, rateCodes, productOptions, billingMethod, term, usageLower, usageMiddle, usageUpper)
+	cobra.CheckErr(err)
+
+	id, err := res.LastInsertId()
+	cobra.CheckErr(err)
+
+	fmt.Println("Id inserted: " + strconv.FormatInt(id, 10))
+
+	//	fmt.Printf(`Contract Start Month: %s
+	//State: %s
+	//Utility: %s
+	//Zone: %s
+	//Rate Code(s): %s
+	//Product Special Options: %s
+	//Billing Method: %s
+	//Term: %s
+	//0-49: %s
+	//50-299: %s
+	//300-1099: %s`, contractStart, state, utility, zone, rateCodes, productOptions, billingMethod, term, usageLower, usageMiddle, usageUpper)
 	return true
 }
