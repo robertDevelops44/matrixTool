08882bf15089241f946eeb15cf8198fc514bd9c9
rh5661 <rh5661@rit.edu>
2023-08-07 11:14:59 -0400

    command functionality added - description & input guidelines

--- cmd/setDualBilling.go
+++ cmd/setDualBilling.go
@@ -5,27 +5,54 @@ package cmd
 
 import (
 	"fmt"
-
+	"github.com/rh5661/matrixTool/pkg/dbModify"
 	"github.com/spf13/cobra"
+	"strconv"
+	"strings"
 )
 
 // setDualBillingCmd represents the setDualBilling command
-var setDualBillingCmd = &cobra.Command{
-	Use:   "setDualBilling",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+var (
+	dualBilling string
+	dualParam   bool
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("setDualBilling called")
-	},
-}
+	setDualBillingCmd = &cobra.Command{
+		Use:   "setDualBilling",
+		Short: "Set the parameter to include dual billing",
+		Long: `Dual Billing inclusion will be updated to inputted parameter
+Any Reports generated will be under this parameter
+Yes/True will include Dual Billing, No/False will generate a report without dual billing
+Please write the parameter in the format "True""/"False", "T"/"F"", "Yes"/"No", or "Y"/"N"
+Example usage:
+matrixTool setDualBilling "No"
+matrixTool setDualBilling "true"
+matrixTool setDualBilling "T"`,
+
+		Run: func(cmd *cobra.Command, args []string) {
+			if dualBilling == "" && len(args) != 0 {
+				dualBilling := strings.ToLower(args[0])
+				if dualBilling == "false" || dualBilling == "f" || dualBilling == "no" || dualBilling == "n" {
+					dualParam = false
+					fmt.Println("The entered dual billing option is: " + dualBilling + " = " + strconv.FormatBool(dualParam) + "\n")
+				} else if dualBilling == "true" || dualBilling == "t" || dualBilling == "yes" || dualBilling == "y" {
+					dualParam = true
+					fmt.Println("The entered dual billing option is: " + dualBilling + " = " + strconv.FormatBool(dualParam) + "\n")
+				} else {
+					fmt.Println("Please specify a dual billing option with the correct format. Run 'matrixTool setDualBilling --help' for more information.")
+					return
+				}
+			} else {
+				fmt.Println("Please specify a dual billing option. Run 'matrixTool setUtil --help' for more information.")
+				return
+			}
+			dbModify.SetDualBilling(dualParam)
+		},
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(setDualBillingCmd)
+	rootCmd.PersistentFlags().StringVarP(&dualBilling, "dualBilling", "", "", "[Required] Set dualBilling inclusion of matrix pricing desired")
 
 	// Here you will define your flags and configuration settings.
 
