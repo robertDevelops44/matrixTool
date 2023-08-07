d7f69d30b4db24f8bba3970f2b9f22d7ca9cb36b
rh5661 <rh5661@rit.edu>
2023-08-07 14:50:19 -0400

    command functionality added - description & input guidelines

--- cmd/setTerms.go
+++ cmd/setTerms.go
@@ -5,27 +5,53 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/pquerna/ffjson/ffjson"
+	"github.com/rh5661/matrixTool/pkg/dbModify"
+	"regexp"
 
 	"github.com/spf13/cobra"
 )
 
 // setTermsCmd represents the setTerms command
-var setTermsCmd = &cobra.Command{
-	Use:   "setTerms",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+var (
+	terms string
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("setTerms called")
-	},
-}
+	setTermsCmd = &cobra.Command{
+		Use:   "setTerms",
+		Short: "A brief description of your command",
+		Long: `Terms entered will be updated to inputted parameter
+Any Reports generated will be under this terms
+Please enter the terms as "[]" or in a 4ct array format: "[12,24,36,48]", "[]"
+Note: MUST BE a 4 count array - enter duplicate values if needed: "[12,12,14,48]"
+Valid input: matrixTool setTerms "[12,12,36,36]"
+Invalid input: matrixTool setTerms "[12,36]"`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if terms == "" && len(args) != 0 {
+				terms = args[0]
+				regex := regexp.MustCompile("^[[0-9]+,[0-9]+,[0-9]+,[0-9]+\\]$")
+				res := regex.MatchString(terms)
+				if terms != "[]" {
+					if !res {
+						fmt.Println("Please specify terms with the correct format. Run 'matrixTool setTerms --help' for more information.")
+						return
+					}
+				}
+				fmt.Println("The entered terms code is: " + terms + "\n")
+			} else {
+				fmt.Println("Please specify terms. Run 'matrixTool setTerms --help' for more information.")
+				return
+			}
+			var termsIntSlice []int
+			err := ffjson.Unmarshal([]byte(terms), &termsIntSlice)
+			cobra.CheckErr(err)
+			dbModify.SetTerms(termsIntSlice)
+		},
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(setTermsCmd)
+	rootCmd.PersistentFlags().StringVarP(&terms, "terms", "", "", "[Required] Set terms of matrix pricing desired")
 
 	// Here you will define your flags and configuration settings.
 
