c001aa42b0b36a3bc003bd76d29f69b6b164854b
rh5661 <rh5661@rit.edu>
2023-08-08 15:17:03 -0400

    generate no longer requires a filepath in command

--- cmd/generate.go
+++ cmd/generate.go
@@ -12,43 +12,40 @@ import (
 )
 
 // generateCmd represents the generate command
-var (
-	filePathExcel string
-
-	generateCmd = &cobra.Command{
-		Use:   "generate",
-		Short: "A brief description of your command",
-		Long: `A longer description that spans multiple lines and likely contains examples
+var generateCmd = &cobra.Command{
+	Use:   "generate",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			if filePathExcel == "" && len(args) != 0 {
-				filePathExcel = args[0]
-				fmt.Println("The entered filePath is: " + filePathExcel + "\n")
-			} else {
-				fmt.Println("Please specify a filepath. Run 'matrixTool generate --help' for more information.")
-				return
-			}
+	Run: func(cmd *cobra.Command, args []string) {
+		parameters := dbModify.ReadJson()
+		filePathExcel := parameters.FilePath
+		if filePathExcel != "" {
+			fmt.Println("The filePath of the excel file is: " + filePathExcel + "\n")
+		} else {
+			fmt.Println("Please specify a filepath with load. Run 'matrixTool load --help' for more information.")
+			return
+		}
 
-			excel.ReadExcelFile(filePathExcel)
-			fmt.Println("\nFinished loading")
-			entries := dbModify.GetFilteredEntries()
-			dbModify.InsertMargin(entries)
-			fmt.Println()
-			for _, entry := range entries {
-				fmt.Printf("%+v\n", entry)
-			}
-			excel.WriteReport(filePathExcel, entries)
-		},
-	}
-)
+		excel.ReadExcelFile(filePathExcel)
+		fmt.Println("\nFinished loading")
+		entries := dbModify.GetFilteredEntries()
+		dbModify.InsertMargin(entries, parameters.Mils)
+		fmt.Println()
+		for _, entry := range entries {
+			fmt.Printf("%+v\n", entry)
+		}
+		excel.WriteReport(filePathExcel, entries)
+	},
+}
 
 func init() {
 	rootCmd.AddCommand(generateCmd)
-	loadCmd.PersistentFlags().StringVarP(&filePathExcel, "filePathExcel", "", "", "[Required] Set filepath of matrix pricing Excel file to be parsed")
+	//loadCmd.PersistentFlags().StringVarP(&filePathExcel, "filePathExcel", "", "", "[Required] Set filepath of matrix pricing Excel file to be parsed")
 
 	// Here you will define your flags and configuration settings.
 
