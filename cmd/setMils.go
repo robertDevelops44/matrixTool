e7470d9ee2c689cb0c11066751e965d1c0c34429
rh5661 <rh5661@rit.edu>
2023-08-09 12:02:18 -0400

    command functionality added - description & input guidelines

--- cmd/setMils.go
+++ cmd/setMils.go
@@ -5,27 +5,46 @@ package cmd
 
 import (
 	"fmt"
-
+	"github.com/rh5661/matrixTool/pkg/dbModify"
 	"github.com/spf13/cobra"
+	"strconv"
 )
 
 // setMilsCmd represents the setMils command
-var setMilsCmd = &cobra.Command{
-	Use:   "setMils",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
+var (
+	milsString string
+
+	setMilsCmd = &cobra.Command{
+		Use:   "setMils",
+		Short: "A brief description of your command",
+		Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("setMils called")
-	},
-}
+		Run: func(cmd *cobra.Command, args []string) {
+			if milsString == "" && len(args) != 0 {
+				milsString = args[0]
+				mils, err := strconv.ParseFloat(milsString, 32)
+				if err != nil {
+					fmt.Println("Please enter a valid number. Run matrixTool setMils --help for more information.")
+					return
+				}
+				dbModify.SetMils(float32(mils))
+
+				fmt.Println("The entered amount of mils is: " + milsString + "\n")
+			} else {
+				fmt.Println("Please specify a mils. Run 'matrixTool setMils --help' for more information.")
+				return
+			}
+		},
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(setMilsCmd)
+	setMilsCmd.PersistentFlags().StringVarP(&milsString, "milsString", "", "", "[Required] Set mils of matrix pricing desired")
 
 	// Here you will define your flags and configuration settings.
 
